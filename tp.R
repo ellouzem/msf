8e25cc824f2d25267e8d9fe3acbb7232c0755dbc
Amine Er-roundi <amine.er-roundi@grenoble-inp.org>
2019-03-04 21:37:20 +0100

    stat2

--- tp.R
+++ tp.R
@@ -17,18 +17,57 @@ corrJ = pacf(RentJdf[,2]^2)
 
 
 ####### Question 7 
-arch <- garch(RentJdf[,2], c(0,11))
+arch11 <- garch(RentJdf[,2], c(0,11))
+Rarch11<-1-var(arch11$residuals[!is.na(arch11$residuals)])/var(RentJdf[,2])
+summary(arch11)
 
-summary(arch)
-Rarch11<-1-var(arch$residuals[!is.na(arch$residuals)])/var(RentJdf[,2])
+arch10 <- garch(RentJdf[,2], c(0,10))
+Rarch10<-1-var(arch10$residuals[!is.na(arch10$residuals)])/var(RentJdf[,2])
+summary(arch9)
+
 
 arch9 <- garch(RentJdf[,2], c(0,9))
 Rarch9<-1-var(arch9$residuals[!is.na(arch9$residuals)])/var(RentJdf[,2])
 summary(arch9)
+
+arch5 <- garch(RentJdf[,2], c(0,5))
+Rarch9<-1-var(arch5$residuals[!is.na(arch5$residuals)])/var(RentJdf[,2])
+summary(arch5)
+
+AIC(arch11)
+AIC(arch10)
+AIC(arch9)
+AIC(arch5)
+
+
+
+arch11fit <- garchFit(~ garch(0,11), RentJdf[periode90_15,2])
+Rarch11<-1-var(arch11$residuals[!is.na(arch11$residuals)])/var(RentJdf[,2])
+summary(arch11fit)
+#### Test
+
+periode90_15 = which(RentJdf[,1] < "2015-01-01")
+arch90_15 = garch(RentJdf[periode90_15,2], c(0,11))
+
+#test
+periode15_18 = which(RentJdf[,1] >= "2015-01-01")
+length(periode15_18)
+pred = predict(arch90_15, RentJdf[periode15_18,2])
+length(pred)
+
 ######### Question 8
 
 
+periode90_04 = which(RentJdf[,1] <= "2004-12-31")
+arch90_04  <- garch(RentJdf[periode90_04,2], c(0,11))
+Rarch90_04 <-1-var(arch90_04 $residuals[!is.na(arch90_04 $residuals)])/var(RentJdf[periode90_04,2])
+summary(arch90_4)
 
+
+periode05_18 = which(RentJdf[,1] > "2004-12-31")
+arch05_18 <- garch(RentJdf[periode05_18,2], c(0,10))
+Rarch05_18<-1-var(arch05_18$residuals[!is.na(arch05_18$residuals)])/var(RentJdf[periode05_18,2])
+summary(arch05_18)
 
 
 
